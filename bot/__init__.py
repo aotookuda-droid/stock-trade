ab40f45e194dc9764417a14ca1ad1a63058878ff
aotookuda-droid <aoto.okuda@gmail.com>
2026-04-14 10:16:01 +0900

    Add __init__.py for bot package

---
+++ bot/__init__.py
@@ -0,0 +1,6 @@
+from bot.trader import StockTrader
+from bot.data_fetcher import DataFetcher
+from bot.strategy import CombinedStrategy
+from bot.portfolio import Portfolio
+
+__all__ = ['StockTrader', 'DataFetcher', 'CombinedStrategy', 'Portfolio']
